f4468b8bc70a1969994a243533775de4a8bf6de6
Aina Tiavina <82655694+Poseidon1402@users.noreply.github.com>
2026-01-05 19:48:33 +0300

    feat: Add AppColor constants
    Introduces an `AppColor` abstract class to provide centralized access to common application color constants. This includes `deepNavyBlue`, `primaryBlueAccent`, and `white`.

---
+++ lib/utils/colors.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+abstract class AppColor {
+  static const Color deepNavyBlue = Color.fromRGBO(10, 22, 38, 1);
+  static const Color primaryBlueAccent = Color.fromRGBO(0, 122, 255, 1);
+  static const Color white = Color.fromRGBO(255, 255, 255, 1);
+}
